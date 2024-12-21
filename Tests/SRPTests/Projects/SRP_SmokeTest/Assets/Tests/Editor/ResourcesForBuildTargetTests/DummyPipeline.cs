128014d5a5236ce97746f9b03bde4a9f521fccd4
Abdulrahmen Almodaimegh <abdulal@bu.edu>
2024-12-20 19:53:02 -0500

    Cleanup 2/2

--- Tests/SRPTests/Projects/SRP_SmokeTest/Assets/Tests/Editor/ResourcesForBuildTargetTests/DummyPipeline.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections.Generic;
-
-namespace UnityEngine.Rendering.DummyPipeline
-{
-    public class DummyPipeline : RenderPipeline
-    {
-        protected override void Render(ScriptableRenderContext context, List<Camera> cameras) { }
-    }
-}
